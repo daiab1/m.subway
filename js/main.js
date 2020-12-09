7e89f7aae3b05f8142be06a67cbcd2b33dfeb781
daiab1 <71070793+daiab1@users.noreply.github.com>
2020-12-10 04:05:48 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('.hotMenuSlider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    centerMode: false    
+        
+             
+});
+	
+})
